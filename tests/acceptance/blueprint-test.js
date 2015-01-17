f1f7f909b918061ab4baf5cae9c86925ff50e244
trabus + jgwhite <trabus+jgwhite@ember-cli.com>
2015-01-17 23:15:45 +0000

    Add first failing test

---
+++ tests/acceptance/blueprint-test.js
@@ -0,0 +1,38 @@
+var Blueprint = require('../../index.js');
+var expect    = require('chai').expect;
+var fs        = require('fs');
+var path      = require('path');
+var root      = process.cwd();
+var tmp       = require('tmp-sync');
+var tmproot   = path.join(root, 'tmp');
+
+describe('Blueprint', function() {
+  var tmpdir;
+  
+  beforeEach(function() {
+    tmpdir = tmp.in(tmproot);  
+  });
+  
+  it('creates the correct files and directories', function() {
+    var exampleBlueprint = new Blueprint('../fixtures/blueprints/example-blueprint');    
+    var model = {
+      name: 'foo'
+    };
+    var options = {
+      destDir: tmpdir
+    };
+    
+    return exampleBlueprint.install(model, options)
+      .then(function() {
+        var filePath, actual
+        
+        filePath = path.join(tmpdir, 'lib/foo.js');
+        actual = fs.readFileSync(filePath, { encoding: 'utf-8' });
+        expect(actual).to.equal('// foo\n');
+        
+        filePath = path.join(tmpdir, 'tests/foo-test.js');
+        actual = fs.readFileSync(filePath, { encoding: 'utf-8' });
+        expect(actual).to.equal('// A test for foo\n');
+      });
+  });
+});
