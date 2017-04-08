5ad1696f5fb8789074e32760bfb9d9a765830f21
Ruslan Grabovoy <kudgo.test@gmail.com>
2017-04-08 16:11:10 +0300

    pass files through editorconfig-fix

--- tests/acceptance/blueprint-test.js
+++ tests/acceptance/blueprint-test.js
@@ -8,6 +8,7 @@ const root      = process.cwd();
 const tmp       = require('tmp-sync');
 const tmproot   = path.join(root, 'tmp');
 const MockUI    = require('console-ui/mock');
+const fixturify = require('fixturify');
 
 describe('Blueprint', function() {
   let tmpdir;
@@ -57,6 +58,69 @@ describe('Blueprint', function() {
         filePath = path.join(tmpdir, 'tests/foo-test.js');
         actual = fs.readFileSync(filePath, { encoding: 'utf-8' });
         expect(actual).to.equal('// A test for foo\n');
+      });
+  });
+
+  it('respects editorconfig', function() {
+    var exampleBlueprint = Blueprint.load('tests-fixtures/blueprints/example-web-app');
+
+    var options = {
+      entity: {
+        name: 'foo'
+      },
+      ui,
+      target: tmpdir,
+      project: {
+        config: function() {
+          return {};
+        },
+        name: function() {
+          return 'foo';
+        },
+        root: tmpdir,
+        // TODO: refactor out
+        isEmberCliProject: function() {
+          return true;
+        },
+        // TODO: refactor out
+        isEmberCLIAddon: function() {
+          return true;
+        }
+      },
+    };
+
+    fixturify.writeSync(tmpdir, {
+      '.editorconfig': `
+root = true
+
+[*]
+indent_style = space
+indent_size = 1
+end_of_line = crlf
+charset = utf-8
+trim_trailing_whitespace = true
+insert_final_newline = true
+`,
+      'app': {
+        '.editorconfig': `
+[*]
+indent_size = 3
+insert_final_newline = false
+`
+      }
+    });
+
+    return exampleBlueprint.install(options)
+      .then(function() {
+        var actual = fixturify.readSync(tmpdir);
+
+        // check project root settings
+        expect(actual.tests['foo-test.js']).to.equal(`if (1) {\r\n // originally poor formatted file\r\n}\r\n`);
+
+        // check folder specific settings and some of filetypes
+        expect(actual.app['foo.js']).to.equal(`if (1) {\r\n   // originally poor formatted file\r\n}`);
+        expect(actual.app['foo.css']).to.equal(`.if {\r\n   color: aquamarine;\r\n}`);
+        expect(actual.app['foo.hbs']).to.equal(`{{#if}}\r\n   originally poor formatted\r\n{{/if}}`);
       });
   });
 });
